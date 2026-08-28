2364c1110ead8c79ee86f692aae242943099c154
Gordon <gordonjb@users.noreply.github.com>
2026-08-28 15:14:14 +0100

    style(py): Fix linter suggestions in `reqs.py`
    Now my linter warnings are working, I've addressed the outstanding
    errors in the existing requests utility class. I'll take a pass at
    main and parse in a future commit.

--- py/reqs.py
+++ py/reqs.py
@@ -1,9 +1,13 @@
+import logging
+from contextlib import closing
+
+import requests_cache
+from requests import Response
 from requests.adapters import HTTPAdapter
 from requests.exceptions import RequestException
-from contextlib import closing
-import requests_cache
+from requests.sessions import Session
 from urllib3 import Retry
-import logging
+
 logger = logging.getLogger(__name__)
 
 
@@ -19,7 +23,7 @@ def get_session() -> requests_cache.CachedSession:
     session.mount("https://", adapter)
     return session
 
-def get_text(session, url) -> bytes:
+def get_text(session:Session, url:str) -> bytes | None:
     """
     Attempts to get the content at `url` by making an HTTP GET request.
     If the content-type of response is some kind of HTML/XML, return the
@@ -27,22 +31,26 @@ def get_text(session, url) -> bytes:
     """
     try:
         with closing(session.get(url)) as resp:
-            logger.info("{cached} URL '{req_url}'".format(req_url=url, cached="Cached" if resp.from_cache else "Uncached"))
+            logger.info("{cached} URL '{req_url}'".format(
+                req_url=url,
+                cached=
+                    "Cached" if (isinstance(resp, requests_cache.CachedResponse) and resp.from_cache)
+                    else "Uncached"))
             if is_good_response(resp):
                 return resp.content
             else:
                 return None
 
     except RequestException as e:
-        logger.error('Error during requests to {0} : {1}'.format(url, str(e)))
+        logger.error(f'Error during requests to {url} : {e!s}')
         return None
 
 
-def is_good_response(resp) -> bool:
+def is_good_response(resp:Response) -> bool:
     """
     Returns True if the response seems to be HTML, False otherwise.
     """
-    content_type = resp.headers['Content-Type'].lower()
+    content_type = resp.headers.get('Content-Type')
     return (resp.status_code == 200
             and content_type is not None
-            and content_type.find('html') > -1)
+            and content_type.lower().find('html') > -1)
